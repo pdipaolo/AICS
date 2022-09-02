2f5f7ead44a90166c2df30568d0569c76f976e26
pdipaolo <>
2022-09-02 18:23:54 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React, {type PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    </SafeAreaView>
+  );
+}; 
+
+export default App;
